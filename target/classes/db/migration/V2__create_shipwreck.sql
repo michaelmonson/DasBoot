24f83b0729c623c3a181be17822a8910498e3b4d
Michael Monson <michaelmonson@ldschurch.org>
2016-03-21 15:40:13 -0600

    Integrating Flyway DB configuration and functionality.

---
+++ target/classes/db/migration/V2__create_shipwreck.sql
@@ -0,0 +1,10 @@
+CREATE TABLE SHIPWRECK (
+	id INT AUTO_INCREMENT,
+	name VARCHAR(255),
+	description VARCHAR(2000),
+	condition VARCHAR(255),
+	depth INT,
+	latitude DOUBLE,
+	longitude DOUBLE,
+	year_Discovered INT
+)
